b868c93289007f08a549b917a033099e07e6d953
vaibhavjs <vaibhavg1201@gmail.com>
2022-01-22 22:59:51 +0530

    Create 136.js

---
+++ 136 - Single Number/136.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = function(nums) {
+    let obj = {};
+    
+    for(let i=0; i<nums.length; i++) {
+        if(obj[nums[i]] == undefined) {
+            obj[nums[i]] = 1;
+        }
+        else {
+            let prev = obj[nums[i]];
+            obj[nums[i]] = prev+1;
+        }
+    }
+    
+    for(key in obj) {
+        if(obj[key] == 1) {
+            return key;
+        }
+    }
+};
